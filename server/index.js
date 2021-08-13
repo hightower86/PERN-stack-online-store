381dd37af165a17bea71ac15ac712424361976d6
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2021-08-13 22:40:20 +0300

    initial commit db configuration file added

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000 
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
