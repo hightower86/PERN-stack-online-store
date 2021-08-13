7716df905933ce36b8da4feba32a4790e5b83818
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2021-08-13 22:59:01 +0300

    db connected

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 require('dotenv').config()
+const sequelize = require('./db')
+
+const PORT = process.env.PORT || 5000
+
 
 const app = express()
 
-const PORT = process.env.PORT || 5000 
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+start()
+
+
