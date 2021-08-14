b8e35040f46102ed1d7b6e2c6a4edacabc9d89a0
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2021-08-14 11:36:50 +0300

    models mistakes corrected. tables in db cteated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const sequelize = require('./db')
+const models = require('./models/models')
 
 const PORT = process.env.PORT || 5000
 
--- server/models/models.js
+++ server/models/models.js
@@ -13,11 +13,11 @@ const Basket = sequelize.define('basket', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 })
 
-const BasketDevice = sequelize.define('basket', {
+const BasketDevice = sequelize.define('basket_device', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 })
 
-const Device = sequelize.define('basket', {
+const Device = sequelize.define('device', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING,unique:true,allowNull:false },
     price: { type: DataTypes.INTEGER,allowNull:false },
@@ -26,25 +26,25 @@ const Device = sequelize.define('basket', {
 })
 
 const DeviceInfo = sequelize.define('device_info', {
-    id: { type: sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    title: { type: sequelize.DataTypes.STRING, unique: true, allowNull: false },
-    description: {type :sequelize.DataTypes.STRING}
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: { type: DataTypes.STRING, unique: true, allowNull: false },
+    description: {type :DataTypes.STRING}
 })
 
 const Rating = sequelize.define('rating', {
-    id: { type: sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    rating: {type:sequelize.DataTypes.INTEGER, defaultValue:0}
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    rating: {type:DataTypes.INTEGER, defaultValue:0}
 })
 const Type = sequelize.define('type', {
-    id: { type: sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: {type:sequelize.DataTypes.STRING, allowNull:false}
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: {type:DataTypes.STRING, allowNull:false}
 })
 const Brand = sequelize.define('brand', {
-    id: { type: sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: {type:sequelize.DataTypes.STRING, allowNull:false}
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: {type:DataTypes.STRING, allowNull:false}
 })
 const TypeBrand = sequelize.define('type_brand', {
-    id: { type: sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 })
 
 User.hasOne(Basket)
