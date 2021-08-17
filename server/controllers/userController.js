2baee0a0c942e529bb2441d9b0cce58d6a52711b
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2021-08-17 20:54:49 +0300

    user registration works

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,36 @@
+const bcrypt = require('bcrypt')
+const {User,Basket} = require('../models/models')
 const ApiError = require('../error/ApiError')
+const jwt = require('jsonwebtoken')
 
 class UserController {
 
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const { email, password, role } = req.body
+
+        if (!email || !password) {
+            return next(ApiError.badRequest('email and password are required'))
+        }
+
+        const candidate = await User.findOne({ where: { email } })
         
-        res.status(200).json({messgage:'registration works'})
+        if (candidate) {
+            return next(ApiError.badRequest('This email address is already registered'))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        
+        const user = await User.create({ email, role, password: hashPassword })
+        
+        const basket = await Basket.create({userId:user.id})
+        
+        const token = jwt.sign(
+            { id: user.id, email, role },
+            process.env.SECRET_KEY,
+            { expiresIn: '24h' }
+        )
+        
+        return res.json({ token })
     }
 
     async login(req, res) {
