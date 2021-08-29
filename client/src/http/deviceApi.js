3c48ea9e3ed04ff0802d5f0d62ba91c75867a7fb
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2021-08-29 16:47:33 +0300

    deviceApi. cteate and fetch types

---
+++ client/src/http/deviceApi.js
@@ -0,0 +1,12 @@
+import { $host, $authHost } from "./index";
+import jwt_decode from "jwt-decode";
+
+export const createType = async (type) => {
+  const { data } = await $authHost.post("api/type", type);
+  return data;
+};
+
+export const fetchTypes = async () => {
+  const { data } = await $host.get("api/type");
+  return data;
+};
